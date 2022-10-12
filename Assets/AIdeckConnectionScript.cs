ad2a460249135a5b0794986896d6b0cfad6f9e4b
Edward Humes <ehumes2@umbc.edu>
2022-10-12 18:26:58 -0400

    Initial commit, streaming works, although not particularly pretty

---
+++ Assets/AIdeckConnectionScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using static ViewerData;
+
+public class AIdeckConnectionScript : MonoBehaviour
+{
+    private TMP_InputField aideck_ip_field;
+    private Button connection_button;
+
+    private const int AIDECK_PORT = 5000;
+
+    void OnEnable()
+    {
+        var canvas = GetComponentInParent<Canvas>().rootCanvas;
+
+        aideck_ip_field = canvas.GetComponent<TMP_InputField>();
+        connection_button = canvas.GetComponent<Button>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void connect_to_aideck()
+    {
+        Socket socket = connect_socket("192.168.4.1", AIDECK_PORT);
+
+        if (socket != null)
+        {
+            ViewerData.aideck_socket = socket;
+            SceneManager.LoadScene("StreamScene", LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.Log("Failed to connect");
+        }
+    }
+
+    private Socket connect_socket(string ip, int port)
+    {
+        IPHostEntry host_entry = Dns.GetHostEntry(ip);
+        IPEndPoint ip_endpoint = new IPEndPoint(host_entry.AddressList[0], port);
+
+        Socket socket = new Socket(ip_endpoint.AddressFamily,
+                                   SocketType.Stream, ProtocolType.Tcp);
+
+        socket.Connect(ip_endpoint);
+
+        if (socket.Connected)
+        {
+            return socket;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/ViewerData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public static class ViewerData
+{
+    public static Socket aideck_socket;
+}
